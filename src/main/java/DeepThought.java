09b5389514a252e04ca5bff3c71219dc941cca51
mrahofer <mrahofer@htl-steyr.ac.at>
2023-11-16 09:08:35 +0100

    enter Test

--- src/main/java/DeepThought.java
+++ src/main/java/DeepThought.java
@@ -2,6 +2,7 @@ import java.util.concurrent.TimeUnit;
 
 public class DeepThought {
     public DeepThought(){
+
     }
 
     public int answer_to_the_ultimate_question_of_life_the_universe_and_everything(){
